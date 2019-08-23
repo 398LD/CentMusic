ae282b1719e983e513e07a87501c9b3d59a50f80
398LD <1813723783@qq.com>
2019-08-24 00:54:35 +0800

    2019-08-24

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 //var serverLocation = "http://192.168.43.84:8080"
 //var serverLocation = "http://192.168.137.237:8080"
-var serverLocation = "http://192.168.100.1:8080"
+var serverLocation = "http://kexun.iok.la"
