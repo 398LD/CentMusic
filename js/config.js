9748c8ebb3e42f4818540bd9e4661397327b4cda
始梦少年 <47942732+398LD@users.noreply.github.com>
2021-01-22 16:13:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 //var serverLocation = "http://192.168.43.84:8080"
 //var serverLocation = "http://192.168.137.237:8080"
-var serverLocation = "http://47.96.174.217"
+var serverLocation = "http://47.96.174.217:8080"
